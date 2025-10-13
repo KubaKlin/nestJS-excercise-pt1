50bb13f8682b9f6307259c00e6a5d69bf044fe38
Kuba Klin <poczta@kubaklin.pl>
2025-10-13 18:15:16 +0200

    feat: api getbyId, delete, patch

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Param, ParseIntPipe, Delete, Patch } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import type { NoteDto } from './note.dto';
 
@@ -12,5 +12,20 @@ export class NotesController {
   @Post()
   create(@Body() note: NoteDto) {
     return this.notesService.create(note);
+  }
+
+  @Get(':id')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return this.notesService.getById(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    await this.notesService.delete(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() note: NoteDto) {
+    return this.notesService.update(id, note);
   }
 }
--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
 import { NoteDto } from './note.dto';
+import { Prisma } from '../../generated/prisma';
+import { PrismaError } from '../database/prisma-error.enum';
 
 @Injectable()
 export class NotesService {
@@ -15,5 +17,57 @@ export class NotesService {
     return this.prismaService.note.create({
       data: note,
     });
+  }
+
+  async getById(id: number) {
+    const note = await this.prismaService.note.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!note) {
+      throw new NotFoundException();
+    }
+    return note;
+  }
+
+  async delete(id: number) {
+    try {
+      return await this.prismaService.note.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error: unknown) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === PrismaError.RecordDoesNotExist
+      ) {
+        throw new NotFoundException();
+      }
+      throw error;
+    }
+  }
+
+  async update(id: number, note: NoteDto) {
+    try {
+      return await this.prismaService.note.update({
+        data: {
+          ...note,
+          id: undefined,
+        },
+        where: {
+          id,
+        },
+      });
+    } catch (error: unknown) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === PrismaError.RecordDoesNotExist
+      ) {
+        throw new NotFoundException();
+      }
+      throw error;
+    }
   }
 }
