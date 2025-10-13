53816d04f5a250a775617b0fd0056a984c096444
Kuba Klin <poczta@kubaklin.pl>
2025-10-13 18:09:50 +0200

    repo init

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { NotesModule } from './notes/notes.module';
+
+@Module({
+  imports: [
+    NotesModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
